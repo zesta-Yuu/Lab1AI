4a6bb3334c3c9f977be2cc2bddfaf36b733eaca7
Zesta-Yuu <27hafza@gmail.com>
2025-09-26 21:35:59 -0600

    test

--- Task_2/jsontest.py
+++ Task_2/jsontest.py
@@ -4,13 +4,13 @@ import os
 
 path="data"
 
-file_name = "game-of-thrones-characters-groups.json"
+#file_name = "game-of-thrones-characters-groups.json"
 json_files = [os.path.join(root, name) 
               for root, dirs, files in os.walk(path) 
               for name in files 
               if name.endswith((".json"))] #If we needed to read several files extensions: if name.endswith((".ext1", ".ext2"))
 
-#print('Number of JSON files ready to be loaded: ' + str(len(json_files)))
+print('Number of JSON files ready to be loaded: ' + str(len(json_files)))
 
 #print(json_files)
 
@@ -18,8 +18,11 @@ json_files = [os.path.join(root, name)
 
 #Open the file using the name of the json file witn open() function
 #Read the json file using load() and put the json data into a variable.
-with open(json_files[0]) as f:
-   json_data = json.load(f)
+if len(json_files) ==1:
+   with open(json_files[0]) as f:
+      json_data = json.load(f)
+else: print("failed")
+   
 
 #print(json_data)
 #print(json_data.keys()) #the top-level variable
